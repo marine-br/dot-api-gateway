4de2f832540a370281fdbc19ba0c1baab21873ef
Alan Rezende <alanrezendeee@gmail.com>
2024-08-19 09:26:16 -0300

    feat: commit initial

---
+++ cmd/main.go
@@ -0,0 +1,12 @@
+package main
+
+// "github.com/sigespweb22/gogate-api/internal/middlewares"
+// "github.com/sigespweb22/gogate-api/internal/proxy"
+// "github.com/sigespweb22/gogate-api/internal/server"
+// "github.com/go-chi/chi/v5/middleware"
+
+import "fmt"
+
+func main() {
+	fmt.Println("Api Gateway")
+}
